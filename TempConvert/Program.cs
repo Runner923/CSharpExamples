c11ac8fbc93e0a0a2b0bec1fe3140211440cddd3
Runner923 <charles_howard2004@yahoo.com>
2014-11-10 08:13:27 -0600

    adding first Temperature Conversion script

---
+++ TempConvert/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+/* Simple Fahrenheit to Celsius (or vice versa) conversion program */
+/* Charles Howard 18-Sep-2014 */
+namespace TempConvert
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            System.Console.WriteLine("Starting...");
+            Temperature temp = new Temperature();
+             Console.WriteLine("Enter Temperature Scale");
+             string answ = Console.ReadLine();
+             answ = answ.ToUpper();
+             if (answ != "F")
+              { temp.TempScale = "C";}
+              else
+              { temp.TempScale = "F";}
+            //Note: Any value that is not explicitly "F" is assumed to be "C"
+            Console.WriteLine(temp.TempScale);
+            temp.TempScaleFactor = temp.SelectScale(temp.TempScale);
+            Console.WriteLine(" Input a temperature ");
+            temp.Temp = System.Single.Parse(Console.ReadLine()); 
+            Console.WriteLine(" Temperature to Convert " + temp.Temp);
+            Console.WriteLine(" Scale Factor used " + temp.TempScaleFactor);
+            Console.WriteLine(" temp.TempScale value used " + temp.TempScale);
+            float newtemp = temp.CalcConversion(temp.Temp, temp.TempScaleFactor, temp.TempScale);
+            Console.WriteLine(" Converted Temp is " + newtemp);
+            Console.ReadLine();
+        }
+    }
+}
---
+++ TempConvert/Temperature.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+/* Tempeature Class with three fields and two methods */
+/* Fields: */
+/*    Temp - a temperature value to be converted, input from the console */
+/*    TempScaleFactor - scale factor to apply to the input temperature  */
+/*    TempScale - string indicating whether input temperature is Fahrenheit or Celsius */
+/*  Methods: */
+/*    SelectScale - chooses the scale factor based on value of TempScale */
+/*    CalcConversion - calculates the new temperature based on input temp, input scale, scale factor */
+
+
+namespace TempConvert
+{
+    class Temperature
+    {
+
+        public float Temp;
+        public float TempScaleFactor;
+        public string TempScale;
+        public float SelectScale(string TempScaleFactorValue)
+        {
+            if (TempScaleFactorValue == "F")
+            { return (float) 5 / 9; }
+            else
+            { return (float) 9 / 5; }
+        }
+        public float CalcConversion(float TempValue, float TempScaleFactor, string TempScaleValue)
+        {
+            if (TempScaleValue == "F")
+            {
+                return (float) (TempValue-32) * TempScaleFactor;
+            }
+            else
+            { return (float) TempValue*TempScaleFactor+32; }
+        }
+
+
+/*        public Temperature();*/
+    }
+}
